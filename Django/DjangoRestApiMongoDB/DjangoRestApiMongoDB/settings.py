f575370280de571bb30c6ab8390b3be0bebdb99f
Yatendrajain <yatendrawebdesigner@gmail.com>
2021-07-05 19:48:25 +0530

    Added cors middleware

--- Django/DjangoRestApiMongoDB/DjangoRestApiMongoDB/settings.py
+++ Django/DjangoRestApiMongoDB/DjangoRestApiMongoDB/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
