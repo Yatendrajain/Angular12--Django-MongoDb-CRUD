6a450ccede73fc3a4a4eeb4237b135ec209ffbac
Yatendrajain <yatendrawebdesigner@gmail.com>
2021-07-05 19:17:46 +0530

    added mongodb as database

--- djangorestapimongodb/djangorestapimongo/settings.py
+++ djangorestapimongodb/djangorestapimongo/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django REST framework
+    'rest_framework'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'djangorestapimongo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'angulardjangotutorial',
+        'HOST': '127.0.0.1',
+        'PORT': 27017,
     }
 }
 
