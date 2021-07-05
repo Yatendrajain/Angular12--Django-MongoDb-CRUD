783afd1c159ca03da2929fa7e8a171914b2d5511
Yatendrajain <yatendrawebdesigner@gmail.com>
2021-07-05 19:50:49 +0530

    cors permission allowed to true from different route

--- Django/DjangoRestApiMongoDB/DjangoRestApiMongoDB/settings.py
+++ Django/DjangoRestApiMongoDB/DjangoRestApiMongoDB/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
