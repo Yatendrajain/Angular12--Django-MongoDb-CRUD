86fc0a5bc2e60ee7bc777d89da622d64046edbdb
Yatendrajain <yatendrawebdesigner@gmail.com>
2021-07-06 16:33:40 +0530

    Added Components and service

---
+++ tutorial-angular12/src/app/components/tutorials-list/tutorials-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tutorial-tutorials-list',
+  templateUrl: './tutorials-list.component.html',
+  styleUrls: ['./tutorials-list.component.css']
+})
+export class TutorialsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
